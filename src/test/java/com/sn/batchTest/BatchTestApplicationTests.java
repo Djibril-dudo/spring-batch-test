91ab8c5229b6f79c4757a5e4a1e56b3d939db1ab
Djibril-dudo <diouf920410@gmail.com>
2024-05-13 14:38:04 +0000

    first commit

---
+++ src/test/java/com/sn/batchTest/BatchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sn.batchTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
